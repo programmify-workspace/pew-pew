8f797592f490c7ed003f6dcae5a036ceaabba1a8
jkavod <ubongjosevv@gmail.com>
2024-07-20 15:35:26 +0100

    mysql sample added

---
+++ Project 2/server/mysqlsample.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE todolist;
+
+USE todolist;
+
+CREATE TABLE todos (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  completed BOOLEAN NOT NULL DEFAULT false
+);
